440fe2940359655b3b3bb0e6ef1c491e8a110afa
oliver-csu <oliver.csu@gmail.com>
2020-02-06 14:28:35 +0800

    Gradle Init

---
+++ src/test/java/ren/oliver/gradle/GradleApplicationTests.java
@@ -0,0 +1,13 @@
+package ren.oliver.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
